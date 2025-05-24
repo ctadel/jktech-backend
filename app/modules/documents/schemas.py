b975032861cdbe4558b53d9b2799df7bdd2aede5
ctadel <nep.prajwal@gmail.com>
2025-05-25 03:18:22 +0530

    updated the schema for better response documentation

--- app/modules/documents/schemas.py
+++ app/modules/documents/schemas.py
@@ -3,15 +3,15 @@ from typing import Optional
 from datetime import datetime
 
 class UploadDocumentRequest(BaseModel):
-    title: str
+    title: str = 'Harry Potter eBook'
     is_private_document: Optional[bool] = False
 
 class DocumentResponse(BaseModel):
-    id: int
-    document_key: str
-    title: str
-    file_path: str
-    version: int
-    uploaded_at: datetime
-    is_private_document: bool
-    user_id: int
+    id: int = 1
+    document_key: str = '64aaf05e-1fd3-423b-a564-a3c0200408fd'
+    title: str = 'Harry Potter eBook'
+    file_path: str = 's3://somesecretlocation/64aaf05e-1fd3-423b-a564-a3c0200408fd'
+    version: int = 1
+    uploaded_at: datetime = '2025-11-25 10:42:12'
+    is_private_document: bool = False
+    username: int = 1
