5f71485eaf00bb12d2bfbd576c6b74d7dc298e19
ctadel <nep.prajwal@gmail.com>
2025-05-25 03:26:29 +0530

    fix missing git commits

--- app/common/constants.py
+++ app/common/constants.py
@@ -2,3 +2,6 @@ class PaginationConstants:
     USERS_PER_PAGE = 10
     DOCUMENTS_PER_PAGE = 20
 
+class FreeTierLimitations:
+    MAX_UPLOAD_DOCUMENTS = 3
+    DOCUMENT_TOKEN_LIMIT = 100,000
--- app/common/exceptions.py
+++ app/common/exceptions.py
@@ -8,6 +8,9 @@ class InvalidUserParameters(HTTPException):
     def __init__(self, parameter):
         super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Invalid user parameter: {parameter}")
 
+class InvalidAuthToken(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Invalid Token")
 
 class UserNotFoundException(HTTPException):
     def __init__(self):
