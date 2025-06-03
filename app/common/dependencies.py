ca62f74da464437da540ccc55d93602d68e1c481
ctadel <nep.prajwal@gmail.com>
2025-06-03 14:02:56 +0530

    Fixed invalid token checks

--- app/common/dependencies.py
+++ app/common/dependencies.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 
 from app.common.database import get_db
 from app.common.auth import decode_access_token
+from app.common.exceptions import InvalidAuthToken
 from app.modules.users.models import AccountLevel, User
 from app.modules.users import crud as user_crud
 from app.config import settings
@@ -16,12 +17,9 @@ async def get_current_user(
         db: Session = Depends(get_db)
         ) -> User:
     payload = decode_access_token(token)
-    if not payload:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
-
     user = await user_crud.get_user_by_username(db, username=payload.get("username"))
-    if user is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invliad User Token")
+    if not user:
+        raise InvalidAuthToken("The user with this token does not exist")
 
     return user
 
